916c6db995a1e43b4c4048260a683bb2d6eb183d
Kianoush Keshani <kianoush.keshani@studio.unibo.it>
2025-09-24 12:55:19 +0000

    fixed argv

--- data_collection/scripts/02_organizing_metadata.py
+++ data_collection/scripts/02_organizing_metadata.py
@@ -1,6 +1,12 @@
 import pandas as pd
+from sys import argv
 
+'''
 neg_path = "~/project/data_collection/output/eukarya_SP_neg.tsv"
 pos_path = "~/project/data_collection/output/eukarya_SP_pos.tsv"
+'''
+neg_path = argv[1]
+pos_path = argv[2]
+
 unf_neg = pd.read_csv(neg_path, sep = "\t")
 unf_pos = pd.read_csv(pos_path, sep = "\t")
