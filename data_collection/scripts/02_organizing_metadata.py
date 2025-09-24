8575af73564f64865eda39fbf4c9403bb4879a19
Bianca Gustini <bgustini96@gmail.com>
2025-09-24 15:53:08 +0200

    new method for THE tsv

--- data_collection/scripts/02_organizing_metadata.py
+++ data_collection/scripts/02_organizing_metadata.py
@@ -1,9 +1,27 @@
 import pandas as pd
 from sys import argv
 
-neg_path = argv[1]
-pos_path = argv[2]
+neg_file = argv[1]
+pos_file = argv[2]
+neg_id = argv[3]
+pos_id = argv[4]
 
+THE_tsv = argv[5]
+
+def filter_tsv(filename, id_list, newfile):
+    with open(id_list, "r") as ids:
+        with open(filename, "r") as data:
+            with open(newfile, "a") as DAfile: 
+                for line in ids:
+                    for l in data:
+                        if l.startswith(line.rstrip()):
+                            DAfile.write(l)
+                            break
+
+filter_tsv(pos_file,pos_id,THE_tsv)
+filter_tsv(neg_file,neg_id,THE_tsv)
+
+'''
 col_labels = ["UniProt ID", "Species", "Kingdom", "Seq Length", "Cleavage Site", "TransM Helix 90", "T/B", "T_id"]
 unf_neg = pd.read_csv(neg_path, sep = "\t", names = col_labels)
 unf_pos = pd.read_csv(pos_path, sep = "\t",  names = col_labels)
@@ -14,3 +32,11 @@ print(unf_neg.head(5))
 print("")
 print(unf_pos.head(5))
 
+"""
+
+
+unf(r) and fil (w):
+for line in unf:
+    if line.startswith(line id):
+        filter.writeline
+'''
