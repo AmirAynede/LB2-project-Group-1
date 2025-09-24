ecd41801b13090c3ce6f8cbe55a26e8de38a3510
Bianca Gustini <bgustini96@gmail.com>
2025-09-24 15:01:05 +0200

    2nd test of script

--- data_collection/scripts/02_organizing_metadata.py
+++ data_collection/scripts/02_organizing_metadata.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sys import argv
 
-'''
-neg_path = "~/project/data_collection/output/eukarya_SP_neg.tsv"
-pos_path = "~/project/data_collection/output/eukarya_SP_pos.tsv"
-'''
 neg_path = argv[1]
-pos_path = argv[2]
+#pos_path = argv[2]
 
 unf_neg = pd.read_csv(neg_path, sep = "\t")
-unf_pos = pd.read_csv(pos_path, sep = "\t")
+#unf_pos = pd.read_csv(pos_path, sep = "\t")
+
+#def filter_df(dataframe, id_file):
+
+print(unf_neg.head(5))
+
