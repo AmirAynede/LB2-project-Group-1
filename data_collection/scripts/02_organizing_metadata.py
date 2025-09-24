438e0e8fb7331e04cfcf79bf56e8c496e47feaa7
Bianca Gustini <bgustini96@gmail.com>
2025-09-24 15:16:56 +0200

    added col labels

--- data_collection/scripts/02_organizing_metadata.py
+++ data_collection/scripts/02_organizing_metadata.py
@@ -2,13 +2,15 @@ import pandas as pd
 from sys import argv
 
 neg_path = argv[1]
-#pos_path = argv[2]
+pos_path = argv[2]
 
 col_labels = ["UniProt ID", "Species", "Kingdom", "Seq Length", "Cleavage Site", "TransM Helix 90", "T/B", "T_id"]
 unf_neg = pd.read_csv(neg_path, sep = "\t", names = col_labels)
-#unf_pos = pd.read_csv(pos_path, sep = "\t")
+unf_pos = pd.read_csv(pos_path, sep = "\t",  names = col_labels)
 
 #def filter_df(dataframe, id_file):
 
+print(unf_neg.head(5))
+print("")
 print(unf_neg.head(5))
 
