9640643688268ab958f90fbb6ea8b31cf146b19c
Kianoush Keshani <kianoush.keshani@studio.unibo.it>
2025-09-24 13:04:03 +0000

    fix header

--- data_collection/scripts/02_organizing_metadata.py
+++ data_collection/scripts/02_organizing_metadata.py
@@ -4,7 +4,7 @@ from sys import argv
 neg_path = argv[1]
 #pos_path = argv[2]
 
-unf_neg = pd.read_csv(neg_path, sep = "\t")
+unf_neg = pd.read_csv(neg_path, sep = "\t", header=0)
 #unf_pos = pd.read_csv(pos_path, sep = "\t")
 
 #def filter_df(dataframe, id_file):
